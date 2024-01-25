95a8e53dbb66e464d884daf10984f506de8d0ba4
Francisco <fllaryora@gmail.com>
2024-01-25 11:30:27 -0300

    functional code

--- examples/ESP32_MQ135/ESP32_MQ135.ino
+++ examples/ESP32_MQ135/ESP32_MQ135.ino
@@ -3,6 +3,8 @@
 //sudo apt install python3-pip
 //pip3 install pyserial
 
+//ESP32-WROOM-DA MOdule
+//100 during upload
 #include<MQUnifiedsensor.h>
 
 
@@ -14,26 +16,29 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+//SERVICE_UUID
+#define FIRST_UUID "43dc60ce-42a1-11ee-be56-0242ac120001"
+#define SECOND_UUID  "43dc60ce-42a2-11ee-be56-0242ac120001"
+#define THRID_UUID         "43dc60ce-42a3-11ee-be56-0242ac120001"
+#define FOUTH_UUID         "43dc60ce-42a4-11ee-be56-0242ac120001"
 
-#define SERVICE_UUID "43dc60ce-42a1-11ee-be56-0242ac120002"
+#define CARBON_MONOXIDE_CHRC_UUID "43dc60ce-42b1-11ee-be56-0242ac120001"
+#define CARBON_DIOXIDE_CHRC_UUID  "43dc60ce-42b2-11ee-be56-0242ac120001"
+#define ALCOHOL_CHRC_UUID         "43dc60ce-42b3-11ee-be56-0242ac120001"
+#define ACETONE_CHRC_UUID         "43dc60ce-42b4-11ee-be56-0242ac120001"
+#define TOLUENE_CHRC_UUID          "43dc60ce-42b5-11ee-be56-0242ac120001"
+#define AMMONIUM_CHRC_UUID        "43dc60ce-42b6-11ee-be56-0242ac120001"
+#define PERCENTAGE_CHRC_UUID      "43dc60ce-42b7-11ee-be56-0242ac120001"
 
-#define CARBON_MONOXIDE_CHAR_UUID "4e771fe2-42a1-11ee-be56-0242ac120000"
-#define CARBON_DIOXIDE_CHAR_UUID  "4e771fe3-42a2-11ee-be57-0242ac120001"
-#define ALCOHOL_CHAR_UUID         "4e771fe4-42a3-11ee-be56-0242ac120002"
-#define ACETONE_CHAR_UUID         "4e771fe5-42a4-11ee-be57-0242ac120003"
-#define TOLUEN_CHAR_UUID          "4e771fe6-42a5-11ee-be56-0242ac120004"
-#define AMMONIUM_CHAR_UUID        "4e771fe7-42a6-11ee-be58-0242ac120005"
-#define PERCENTAGE_CHAR_UUID      "4e771fe8-42a7-11ee-be56-0242ac120006"
+#define CARBON_MONOXIDE_DESC_UUID 0x3901
+#define CARBON_DIOXIDE_DESC_UUID  0x3902
+#define ALCOHOL_DESC_UUID         0x3903
+#define ACETONE_DESC_UUID         0x3904
+#define TOLUENE_DESC_UUID          0x3905
+#define AMMONIUM_DESC_UUID        0x3906
+#define PERCENTAGE_DESC_UUID      0x3907
 
-#define CARBON_MONOXIDE_DESC_UUID 0x2901
-#define CARBON_DIOXIDE_DESC_UUID  0x2902
-#define ALCOHOL_DESC_UUID         0x2903
-#define ACETONE_DESC_UUID         0x2904
-#define TOLUEN_DESC_UUID          0x2905
-#define AMMONIUM_DESC_UUID        0x2906
-#define PERCENTAGE_DESC_UUID      0x2907
-
-#define DEVICE_NAME "Smoke Detector"
+#define DEVICE_NAME "Air Quality Detector"
 
 #define PIN_MQ135 34 //input only
 
@@ -41,16 +46,20 @@ MQUnifiedsensor mq135_sensor(PIN_MQ135, MAX_VALUE_CONVERSOR_ESP_32);
 
 BLEServer* pServer = NULL;
 
-BLECharacteristic* carbonMonoxideChar = NULL;
-BLECharacteristic* carbonDioxideChar = NULL;
-BLECharacteristic* alcoholChar = NULL;
-BLECharacteristic* acetoneChar = NULL;
-BLECharacteristic* toluenChar = NULL;
-BLECharacteristic* amoniumChar = NULL;
-BLECharacteristic* percentageChar = NULL;
+#define CARBON_MONOXIDE_INDEX 0
+#define CARBON_DIOXIDE_INDEX 1
+#define ALCOHOL_INDEX 2
+#define ACETONE_INDEX 3
+#define TOLUENE_INDEX 4
+#define AMMONIUM_INDEX 5
+#define PERCENTAGE_INDEX 6
 
-BLEDescriptor *pDescr;
-BLE2902 *pBLE2902;
+//BLECharacteristic* percentageChar = NULL;
+BLECharacteristic* arrayCharacteristics [7];
+ 
+
+BLEService* serviceList [4];
+//BLEDescriptor *pDescr;
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
@@ -66,6 +75,46 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
+
+void createService ( char * SERVICE_UUID, int positionIndex){
+ // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  serviceList [positionIndex] = pService;
+
+  // Start the service
+  //pService->start();
+  return;
+}
+
+void startService (int positionIndex){
+  BLEService *pService = serviceList [positionIndex];
+
+  // Start the service
+  pService->start();
+  return;
+}
+
+BLECharacteristic* createCharacteristic (int positionIndex, char* CHAR_UUID, uint16_t DESC_UUID, char* value ){
+  BLEService *pService = serviceList [positionIndex];
+ // Create a BLE Characteristic
+  BLECharacteristic* characteristic = pService->createCharacteristic(
+          CHAR_UUID,
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_READ
+          //BLECharacteristic::PROPERTY_WRITE |
+          //BLECharacteristic::PROPERTY_INDICATE
+  );
+  BLEDescriptor * pDescr = new BLEDescriptor(DESC_UUID);
+  pDescr->setValue(value); // uuid 0000xxxx-0000-1000-8000-00805F9B34FB 
+  characteristic->addDescriptor(pDescr); //additional fixed metadata
+  BLE2902 * pBLE2902 = new BLE2902(); //CCCD uuid 00002902-0000-1000-8000-00805F9B34FB 
+  pBLE2902->setNotifications(true); //this enadle the notifications
+  characteristic->addDescriptor(pBLE2902);
+
+  return characteristic;
+}
+
 void setup() {
   //blue led
   pinMode(2,OUTPUT);
@@ -80,144 +129,60 @@ void setup() {
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
+  createService ( FIRST_UUID, 0);
   // Create a BLE Characteristic
-  carbonMonoxideChar = pService->createCharacteristic(
-          CARBON_MONOXIDE_CHAR_UUID,
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_READ |
-          //BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)CARBON_MONOXIDE_DESC_UUID);
-  pDescr->setValue("Carbon Monoxide PPM");
-  carbonMonoxideChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  carbonMonoxideChar->addDescriptor(pBLE2902);
+  arrayCharacteristics[CARBON_MONOXIDE_INDEX] = createCharacteristic (0, CARBON_MONOXIDE_CHRC_UUID,
+    (uint16_t)CARBON_MONOXIDE_DESC_UUID,"Carbon Monoxide PPM");
+
+  arrayCharacteristics[CARBON_DIOXIDE_INDEX] = createCharacteristic ( 0, CARBON_DIOXIDE_CHRC_UUID,
+    (uint16_t)CARBON_DIOXIDE_DESC_UUID,"Carbon Dioxide PPM");
+
+  startService (0);
+  createService ( SECOND_UUID, 1);
+  arrayCharacteristics[ALCOHOL_INDEX] = createCharacteristic ( 1, ALCOHOL_CHRC_UUID,
+    (uint16_t)ALCOHOL_DESC_UUID,"Alcohol PPM");
+
+  arrayCharacteristics[ACETONE_INDEX] = createCharacteristic ( 1, ACETONE_CHRC_UUID,
+    (uint16_t)ACETONE_DESC_UUID,"Acetone PPM");
+  startService (1);
+createService ( THRID_UUID, 2);
+    
+  arrayCharacteristics[TOLUENE_INDEX] = createCharacteristic ( 2, TOLUENE_CHRC_UUID,
+    (uint16_t)TOLUENE_DESC_UUID,"Toluene PPM");
+
+  arrayCharacteristics[AMMONIUM_INDEX] = createCharacteristic ( 2, AMMONIUM_CHRC_UUID,
+    (uint16_t)AMMONIUM_DESC_UUID,"Amonium PPM");
   
-  carbonDioxideChar = pService->createCharacteristic(
-          CARBON_DIOXIDE_CHAR_UUID,
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_READ |
-          //BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)CARBON_DIOXIDE_DESC_UUID);
-  pDescr->setValue("Carbon Dioxide PPM");
-  carbonDioxideChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  carbonDioxideChar->addDescriptor(pBLE2902);
-
-  alcoholChar = pService->createCharacteristic(
-        ALCOHOL_CHAR_UUID,
-        BLECharacteristic::PROPERTY_NOTIFY |
-        BLECharacteristic::PROPERTY_READ |
-        //BLECharacteristic::PROPERTY_WRITE |
-        BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)ALCOHOL_DESC_UUID);
-  pDescr->setValue("Alcohol PPM");
-  alcoholChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  alcoholChar->addDescriptor(pBLE2902);
-
-  acetoneChar = pService->createCharacteristic(
-          ACETONE_CHAR_UUID,
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_READ |
-          //BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)ACETONE_DESC_UUID);
-  pDescr->setValue("Acetone PPM");
-  acetoneChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  acetoneChar->addDescriptor(pBLE2902);
-
-  toluenChar = pService->createCharacteristic(
-            TOLUEN_CHAR_UUID,
-            BLECharacteristic::PROPERTY_NOTIFY |
-            BLECharacteristic::PROPERTY_READ |
-            //BLECharacteristic::PROPERTY_WRITE |
-            BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)TOLUEN_DESC_UUID);
-  pDescr->setValue("Toluene PPM");
-  toluenChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  toluenChar->addDescriptor(pBLE2902);
-
-  amoniumChar = pService->createCharacteristic(
-                      AMMONIUM_CHAR_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_READ |
-                      //BLECharacteristic::PROPERTY_WRITE |
-                      BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)AMMONIUM_DESC_UUID);
-  pDescr->setValue("Amonium PPM");
-  amoniumChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  amoniumChar->addDescriptor(pBLE2902);
-
- percentageChar = pService->createCharacteristic(
-          PERCENTAGE_CHAR_UUID,
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_READ |
-          //BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_INDICATE
-  );
-  pDescr = new BLEDescriptor((uint16_t)PERCENTAGE_DESC_UUID);
-  pDescr->setValue("Percentage from 0 to 99");
-  percentageChar->addDescriptor(pDescr);
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  percentageChar->addDescriptor(pBLE2902);
-  
-  // Start the service
-  pService->start();
-
+  startService (2);
+  createService ( FOUTH_UUID, 3);
+  arrayCharacteristics[6] = createCharacteristic ( 3, PERCENTAGE_CHRC_UUID,
+    (uint16_t)PERCENTAGE_DESC_UUID,"Percentage");
+  startService (3);
 
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  
+  pAdvertising->addServiceUUID(FIRST_UUID);
+  pAdvertising->addServiceUUID(SECOND_UUID);
+  pAdvertising->addServiceUUID(THRID_UUID);
+  pAdvertising->addServiceUUID(FOUTH_UUID);
+  //it has 6 service max. after this number it add a new char to the last service. 
   pAdvertising->setScanResponse(false);
   //pAdvertising->setScanResponse(true);
 
   /**
   Workaround for improving Bluetooth connection compatibility with iPhones.
-   iPhones can be quite strict when it comes to Bluetooth connection parameters,
-    and sometimes adjustments need to be made to ensure stable connections between an iPhone and a Bluetooth device.
-  
-  The setMinPreferred function is typically used to set the minimum connection interval preferred by the advertising device.
-  The connection interval is the time between two consecutive data exchange events between the Bluetooth devices.
-  It plays a crucial role in determining the responsiveness and power consumption of the connection.
-
-  In the Bluetooth specification, the connection interval is specified in units of 1.25 ms,
-   and its range is between 7.5 ms and 4,000 ms. iPhones are known to be sensitive to the connection interval,
-    and by adjusting this interval to certain values, you can improve the likelihood of a stable connection.
 
   These values (0x06 and 0x12) are hexadecimal representations of the desired minimum connection interval.
    In decimal, these values are 6 and 18, which correspond to connection intervals of 7.5 ms and 18.75 ms, respectively.
   */
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
+  pAdvertising->setMaxPreferred(0x12);
+  
+  mq135_sensor.calibrate();
+  Serial.println("Calibrating...");
 
   BLEDevice::startAdvertising();
-
- 
   Serial.println("Waiting a client connection to notify...");
-  mq135_sensor.calibrate();
-  
 }
 
 
@@ -255,29 +220,29 @@ void loop() {
 
     // notify changed value
     if (deviceConnected) {
-      
+        arrayCharacteristics[CARBON_MONOXIDE_INDEX]->setValue(carbonMonoxide);
+        arrayCharacteristics[CARBON_MONOXIDE_INDEX]->notify();
 
-        carbonMonoxideChar->setValue(carbonMonoxide);
-        carbonMonoxideChar->notify();
+        arrayCharacteristics[CARBON_DIOXIDE_INDEX]->setValue(carbonDioxide);
+        arrayCharacteristics[CARBON_DIOXIDE_INDEX]->notify();
 
-        carbonDioxideChar->setValue(carbonDioxide);
-        carbonDioxideChar->notify();
+        arrayCharacteristics[ALCOHOL_INDEX]->setValue(alcohol);
+        arrayCharacteristics[ALCOHOL_INDEX]->notify();
 
-        alcoholChar->setValue(alcohol);
-        alcoholChar->notify();
+        arrayCharacteristics[ACETONE_INDEX]->setValue(acetone);
+        arrayCharacteristics[ACETONE_INDEX]->notify();
 
-        acetoneChar->setValue(acetone);
-        acetoneChar->notify();
+        arrayCharacteristics[TOLUENE_INDEX]->setValue(toluen);
+        arrayCharacteristics[TOLUENE_INDEX]->notify();
 
-        toluenChar->setValue(toluen);
-        toluenChar->notify();
+        arrayCharacteristics[AMMONIUM_INDEX]->setValue(ammonium);
+        arrayCharacteristics[AMMONIUM_INDEX]->notify();
 
-        amoniumChar->setValue(ammonium);
-        amoniumChar->notify();
+        arrayCharacteristics[6]->setValue(percentage);
+        arrayCharacteristics[6]->notify();
 
-        percentageChar->setValue(percentage);
-        percentageChar->notify();
-
+        Serial.println("percentage");
+        Serial.println(percentage);
         delay(1000);
     }
     // disconnecting
